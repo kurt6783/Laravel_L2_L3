4c4be75668210f732ac7f6333728e900ef027bd9
vagrant <vagrant@homestead>
2020-05-13 15:39:38 +0000

    基礎頁面結構

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="https://www.cakeresume.com/f9759f" target="_blank">kurt</a> 設計和實作 <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:k0911245920@gmail.com">聯繫我們</a></p>
+  </div>
+</footer>
