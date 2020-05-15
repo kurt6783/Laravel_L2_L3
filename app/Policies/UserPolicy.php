4f9230756f1d625ce8799012b9eaf931e360896f
vagrant <vagrant@homestead>
2020-05-15 06:19:22 +0000

    授權訪問

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function upadte(User $currentUser,User $user){
+        return $currentUser->id ===$user->id;
+    }
+
+
+}
