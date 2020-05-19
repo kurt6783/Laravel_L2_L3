f365179fea767d71d90eb13b17a53dea73da61a6
vagrant <vagrant@homestead>
2020-05-19 16:38:13 +0000

    移除錯誤資料

--- app/Models/Replu.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Models;
-
-class Replu extends Model
-{
-    protected $fillable = ['topic_id', 'user_id', 'content'];
-}
--- database/factories/RepluFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Faker\Generator as Faker;
-
-$factory->define(App\Models\Replu::class, function (Faker $faker) {
-    return [
-        // 'name' => $faker->name,
-    ];
-});
