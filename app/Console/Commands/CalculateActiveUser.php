614ab0082f58dec8c34d24f53ab35ba7ba3892cd
vagrant <vagrant@homestead>
2020-05-25 09:48:34 +0000

    活躍用戶

---
+++ app/Console/Commands/CalculateActiveUser.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+
+class CalculateActiveUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'larabbs:calculate-active-user';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '生成活躍用戶';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle(User $user)
+    {
+        //
+        $this->info("開始計算...");
+
+        $user->calculateAndCacheActiveUsers();
+
+        $this->info("生成成功!");
+    }
+}
