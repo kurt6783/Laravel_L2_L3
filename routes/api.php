45ec19610f6fd7e8e5feb9ae6893abeabcf7fcb6
vagrant <vagrant@homestead>
2020-06-08 11:06:56 +0000

    用戶註冊

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,11 @@
 
 use Illuminate\Http\Request;
 
-Route::prefix('v1')->name('api.v1.')->group(function () {
-    // 短信验证码
-    Route::post('verificationCodes', 'VerificationCodesController@store')
-        ->name('verificationCodes.store');
-});
-
 Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
     // 短信验证码
     Route::post('verificationCodes', 'VerificationCodesController@store')
         ->name('verificationCodes.store');
+
+    Route::post('users', 'UsersController@store')
+        ->name('users.store');
 });
